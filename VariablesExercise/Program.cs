66bc44f7dff8ed25092bfe97c1a071ec25a1586e
Brandon Holliday <brandonholliday1@gmail.com>
2023-12-28 05:35:24 -0700

    Finished exercise

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string myName = "Brandon";
+            int myAge = 30;
+            char myPunctuation = '.';
+            bool isLearning = true;
+            double height = 6.4;
+            decimal weight = 190.2m;
+            Console.WriteLine($"Hello, my name is {myName}. My age is {myAge}{myPunctuation} Am I currently learning at TrueCoders? {isLearning}. My height is {height} feet and my weight is {weight} pounds.");
         }
     }
 }
